2c6c2f41360b0fde1a86b7c99f9327d36c0cb84c
Abhisheak Mall <abhi@th3ia.io>
2025-11-14 18:08:12 +0800

    our addy :)

--- server/src/servers/websocket_server.rs
+++ server/src/servers/websocket_server.rs
@@ -111,7 +111,7 @@ async fn handle_socket(
         send_socket_message(&mut socket, msg).await;
         return;
     }
-    let user_address: Address = "0x023a3d058020fb76cca98f01b3c48c8938a22355".parse().unwrap();
+    let user_address: Address = "0x29c5236E38aAD54Bea26D196571B5eB1401AD052".parse().unwrap();
     loop {
         select! {
             recv_result = internal_message_rx.recv() => {
